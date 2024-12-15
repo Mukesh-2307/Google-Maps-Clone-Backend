68d02cf432dee52e8834c82026f2d468f227ae02
MukeshDalai <mukeshdalai.2307@gmail.com>
2024-12-16 04:10:17 +0530

    added footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <>
       <Navbar title="CiSTUP"/>
       <Home/>
+      <Footer/>
       {/* <h1>CiSTUP IISc, Bengaluru</h1> */}
     </>
   );
---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footSuper">
+        <p className="footText">
+          Google Maps Clone | Developed by Mukesh Kumar Dalai
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
